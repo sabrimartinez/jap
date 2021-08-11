5ec00f3fff4e74a9c2044d7ff005499bab33b0b5
sabrimartinez <88408817+sabrimartinez@users.noreply.github.com>
2021-08-11 15:32:45 -0300

    primeros cambios en entrega 1

--- js/init.js
+++ js/init.js
@@ -40,8 +40,45 @@ var getJSONData = function(url){
     });
 }
 
+var listadoCat = [];
+
+function showCategoriesList(array){
+
+    let htmlContentToAppend = "";
+    for(let i = 0; i < array.length; i++){
+        let category = array[i];
+
+        htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + category.imgSrc + `" alt="` + category.description + `" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h4 class="mb-1">`+ category.name +`</h4>
+                        <small class="text-muted">`+"Precio: "+"  " +category.currency +"  "+category.cost+" <br/>"+"  "+"Cantidad disponible: "+"  "+ category.soldCount +` artículos</small>
+                    </div>
+                    <p>${category.description} </p>
+                </div>
+            </div>
+        </div>
+        `
+
+        document.getElementById("lis").innerHTML = htmlContentToAppend;
+    }
+}
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            listadoCat = resultObj.data;
+            //Muestro las categorías ordenadas
+            showCategoriesList(listadoCat);
+        }
+    });
 });
