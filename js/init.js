f50f9f9ba7b33e0ab36097ff2fd59e1142405a83
sabrimartinez <88408817+sabrimartinez@users.noreply.github.com>
2021-09-03 16:23:16 -0300

    entrega2

--- js/init.js
+++ js/init.js
@@ -7,6 +7,9 @@ const PRODUCT_INFO_COMMENTS_URL = "https://japdevdep.github.io/ecommerce-api/pro
 const CART_INFO_URL = "https://japdevdep.github.io/ecommerce-api/cart/987.json";
 const CART_BUY_URL = "https://japdevdep.github.io/ecommerce-api/cart/buy.json";
 
+
+//transition: transform.3s ease-in-out;
+//transform: translate(110%);
 var showSpinner = function(){
   document.getElementById("spinner-wrapper").style.display = "block";
 }
@@ -42,7 +45,7 @@ var getJSONData = function(url){
 
 function nombreUs (){
   var usuario= document.getElementById('usuario').value;
+
+  
   localStorage.setItem('usnombre',usuario);
 }
-
-
--- js/my-profile.js
+++ js/my-profile.js
@@ -3,7 +3,32 @@
 //elementos HTML presentes.
 
     document.addEventListener("DOMContentLoaded", function (e) {
-        document.getElementById('nombreUsuario').innerHTML =`Bienvenido : <img src='img/iconoUsuario.svg'>` +localStorage.getItem('usnombre')
-      });
-      
      
+      if (localStorage.getItem('usnombre') === null||localStorage.getItem('usnombre')===undefined) {
+        window.location = "index.html"; 
+    } else{  
+        document.getElementById('nombreUsuario').innerHTML =
+        `Bienvenido : <img src='img/iconoUsuario.svg'>` +localStorage.getItem('usnombre');
+        }
+      });
+    
+   
+      $("#cierre").click(function(){
+      $(location).attr('href','index.html');
+      localStorage.clear();
+      
+     })
+    
+     //esto es para el menu 
+
+     const menuIc= document.querySelector(".menuIcono");
+     const menu= document.querySelector('.menuNavegacion');
+     
+     menuIc.addEventListener('click',()=>{
+       menu.classList.toggle('spread')
+     })
+     window.addEventListener('click',e=>{
+       if(menu.classList.contains('spread')
+       && e.target != menu && e.target !=menuIc)
+       { menu.classList.toggle('spread')}
+     })
